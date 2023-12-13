5070ae26a0d246938a9140321a251eaf606ef03d
Wojciech Pituła <wojciech@swissborg.com>
2023-12-13 22:08:25 +0100

    Day 13 solution

---
+++ src/main/kotlin/day13/Part1.kt
@@ -0,0 +1,51 @@
+package day13
+
+import utils.Utils
+
+
+fun main() {
+    val input = Utils.loadResource("/day13.txt")
+    val parsed = parseInput(input)
+    val result = parsed.sumOf { pattern ->
+        val horizontal = pattern.findReflection()?.plus(1)?.times(100)
+        val vertical = pattern.transposed().findReflection()?.plus(1)
+        val result = vertical ?: horizontal
+        println(result)
+        result!!
+    }
+    println(result)
+}
+
+typealias Input = List<Pattern>
+
+data class Pattern(val raw: List<String>) {
+    fun transposed() = Pattern(transpose(raw))
+
+    fun findReflection(): Int? = (0..(raw.size - 2)).find { checkIfReflection(this, it) }
+}
+
+fun parseInput(input: String): Input {
+    val patterns = input.split("\n\n")
+    return patterns.map { Pattern(it.lines()) }
+}
+
+
+fun transpose(input: List<String>): List<String> {
+    if (input.isEmpty()) return emptyList()
+    val rowLength = input.first().length
+    return (0 until rowLength).map { column ->
+        input.joinToString(separator = "") { it[column].toString() }
+    }
+}
+
+fun checkIfReflection(pattern: Pattern, index: Int): Boolean {
+    fun step(distance: Int): Boolean {
+        val checkedLine = index - distance
+        val opposite = index + 1 + distance
+        return if (checkedLine < 0 || opposite >= pattern.raw.size) true
+        else if (pattern.raw[checkedLine] == pattern.raw[opposite]) step(distance + 1)
+        else false
+    }
+    return step(0)
+}
+
